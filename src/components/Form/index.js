abc9bfbdb0572a9457d0aa778b9c593e02f70289
PedroBozan <pedroobozan@gmail.com>
2023-05-31 22:05:26 -0300

    Commit do projeto

---
+++ src/components/Form/index.js
@@ -0,0 +1,13 @@
+import './Form.css';
+
+const Form = () => {
+    return (
+        <section className='result-form'>
+            <form>
+                <input type='text'/>
+            </form>
+        </section>
+    )
+}
+
+export default Form
---
+++ src/components/LocationField/index.js
@@ -0,0 +1,11 @@
+import './LocationField.css';
+
+const LocationField = () => {
+    return (
+        <section className='location-field'>
+            <input type='text' placeholder='Digite a sua localização'/>
+        </section>
+    )
+}
+
+export default LocationField
